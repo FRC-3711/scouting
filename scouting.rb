017b6765586880fd6ad08b3e9eb7f38c708c3208
b-mck <bmck@goodkind.local>
2014-04-18 14:05:01 -0700

    first working commit, first round of features

--- scouting.rb
+++ scouting.rb
@@ -5,26 +5,36 @@ Camping.goes :Scouting
 
 module Scouting
   def self.create
-    Mongoid.load!('mongoid.yml')
+    Mongoid.load!('./mongoid.yml')
+    puts Mongoid.sessions
   end
   
   module Models
     class Attribute
       include Mongoid::Document
       
-      field :_id, type: String
+      field :_id, type: String, default: ->{ name.strip.downcase.gsub(/\s+/, '_') }
       field :name, type: String
       field :group, type: String
       field :type, type: String
-      embeds_many :attroptions
-    end
-   
-    class AttrOption
-      include Mongoid::Document
+
+      def self.quick_take
+        return where( :group => 'quick_take')
+      end
+
+      def self.autonomous
+        return where( :group => 'autonomous' )
+      end
+
+      def self.general
+        return where( :group => 'general' )
+      end
       
-      field :value, type: String
-      field :text, type: String
-      embedded_in :attribute
+      def self.teleop
+        return where( :group => 'teleop' )
+      end
+
+      store_in collection: 'attributes'
     end
 
     class Team
@@ -32,21 +42,25 @@ module Scouting
       
       field :_id, type: Integer
       field :name, type: String
-      field :quick_take, type: String
+      field :attrs, type: Hash, default: Hash.new
+      
+      store_in collection: 'teams'
     end
   end
   
   module Controllers
     class Index
       def get
-        @teams = Team.all
+        @teams = Team.count > 0 ? Team.all : nil
         render :index
       end
     end
     
-    class TeamX
+    class TeamN
       def get(id)
-        @team = Team.first( :_id => id.to_i )
+        @team = Team.where( :_id => id.to_i ).first
+        @quick_take = Attribute.where( :group => "quicktake" ).first
+        @attributes = Attribute.all
         render :team
       end
     end
@@ -54,55 +68,71 @@ module Scouting
     class TeamEdit
       def get
         @edit = true
-        render :team
-      end
-
-      def post
-        @content = @input.content
-      end
-    end
-    
-    class TeamXEdit
-      def get(id)
-        @team = Team.first( :_id => id.to_i )
+        @team = Team.new
+        @quick_take = Attribute.where( :group => "quicktake" ).first
         @attributes = Attribute.all
-        @edit = true
         render :team
       end
       
       def post
-        @content = @input.content
+        attrs = Hash.new
+        Attribute.all.each do |at|
+                       attrs[at._id] = @input[at._id]
+                     end
+        team = Team.new(
+                 :name => @input.team_name,
+                 :attrs => attrs
+               )
+        team._id = @input.team_number
+        team.upsert
+
+        redirect TeamN, team._id
       end
     end
     
-    class TeamEditX
+    class TeamNEdit
       def get(id)
-        @team = Team.first( :_id => id.to_i )
+        @team_number = id.to_i
+        @team = Team.where( :_id => @team_number ).first_or_create
+        @quick_take = Attribute.where( :group => "quicktake" ).first
         @attributes = Attribute.all
         @edit = true
         render :team
       end
-      
-      def post
-        @content = @input.content
+    end
+    
+    class TeamEditN
+      def get(id)
+        @team_number = id.to_i
+        @team = Team.where( :_id => @team_number ).first_or_create
+        @quick_take = Attribute.where( :group => "quicktake" ).first
+        @attributes = Attribute.all
+        @edit = true
+        render :team
       end
     end
-
+    
     class Attributes
       def get
         @attributes = Attribute.all
         render :attributes
       end
-
+      
       def post
         opts = @input.att_options.split("\r\n")
         opt_array = []
         opts.each do |o|
-              opt_array.push(AttrOption.new(:value => o.split(',')[0].strip, :text => o.split(',')[1].strip))
+              opt_array.push( { :value => o.split(',')[0].strip, :text => o.split(',')[1].strip } )
             end
-        att = Attribute.new(:_id => @input.att_id, :name => @input.att_name, :type => @input.att_type, :options => opt_array)
-        att.save
-
+        att = Attribute.new(
+                :_id => @input.att_id,
+                :name => @input.att_name,
+                :type => @input.att_type,
+                :group => @input.att_group,
+                :options => opt_array
+              )
+        att.upsert
+        
         redirect Attributes
       end
     end
@@ -123,21 +153,22 @@ module Scouting
     def index
       @teams.each do |t|
               p do
-                a :href => R(TeamX, t._id) do t._id end
+                a :href => R(TeamN, t._id) do t._id end
                 text " "
-                a :href => R(TeamXEdit, t._id) do "Edit" end
+                a :href => R(TeamNEdit, t._id) do "Edit" end
               end
             end
     end
 
     def team
       if @edit
-        form :action => R(TeamX, @team), :method => :post do
-                                                   team_header(@edit)
-                                                   team_actions(@edit)
-                                                   team_attributes(@edit)
-                                                   team_footer(@edit)
-                                                 end
+        form :action => R(TeamEdit), 
+        :method => :post do
+                  team_header(@edit)
+                  team_actions(@edit)
+                  team_attributes(@edit)
+                  team_footer(@edit)
+                end
       else
         team_header(@edit)
         team_actions(@edit)
@@ -149,56 +180,50 @@ module Scouting
     def team_header(edit)
       div.team_header!.header do
            h1 do
-             if edit           
-               "Team&nbsp;" + input.team_number!.team_number( :type => "number", :value => @team._id )
+             if edit
+               "Team&nbsp;" + input.team_number!.team_number( :type => "number", :name => "team_number", :value => @team._id )
              else
                "Team #{@team._id}"
              end
            end
            h2 do
              if edit
-               input.team_name!.team_name( :type => "text", :value => @team.name )
+               input.team_name!.team_name( :type => "text", :name => "team_name", :value => @team.name )
              else
                @team.name
              end
            end
-           if edit
-             label :for => "quick_take" do
-               "Quick Take:&nbsp;"
-             end
-             select.quick_take! do
-                     quick_take_options
-                   end
-           else
-             text "Quick Take: #{@team.quick_take}"
-           end
-           button.start_match! "Start Match"
+           attribute_input(@quick_take, @team, edit)
          end
     end
     
     def team_actions(edit)
-      div.robot_actions!.actions do
+      div.teleop!.actions do
            h2 "TeleOp"
-           @team.teleop.each do |ax|
-                         if edit
-                           p do
-                             label :for => ax[0] do "#{ax[0]}&nbsp;" end
-                             select :id => ax[0] do
-                               option :value => "unknown", :default => "true"
+           @attributes.teleop.each do |at|
+                               p do
+                                 attribute_input(at, @team, edit)
+                               end
                              end
-                           end
-                         else
-                           p do
-                             text "#{ax[0]}: #{ax[1]}"
-                           end
-                         end
-                       end
+         end
+      div.autonomous!.actions do
+           h2 "Autonomous"
+           @attributes.autonomous.each do |at|
+                                   p do
+                                     attribute_input(at, @team, edit)
+                                   end
+                                 end
          end
     end
-
+    
     def team_attributes(edit)
-      div.robot_attributes!.attributes do
+      div.general!.attributes do
            h2 "General"
+           @attributes.general.each do |at|
+                                p do
+                                  attribute_input(at, @team, edit)
+                                end
+                              end
          end
     end
     
@@ -211,47 +236,119 @@ module Scouting
     end
 
     def attributes
-      if (@attributes)
-        @attributes.each do |at|
-                     p do
-                       text "#{at.name}"
-                     end
-                   end
+      if (@attributes.teleop.exists?)
+        h2 "TeleOp"
+        @attributes.teleop.each do |at|
+                            p do
+                              text "#{at.name} (#{at._id}) / #{at.type}"
+                            end
+                          end
       end
-      
-      form :action => R(Attributes), :method => "post" do
-                                  p do
-                                    label :for => "att_name" do "Name&nbsp;" end
-                                    input.att_name!.att_input :name => "att_name", :type => "text"
-                                    label :for => "att_id" do "&nbsp;ID&nbsp;" end
-                                    input.att_id!.att_input :name => "att_id", :type => "text"
-                                    label :for => "att_section" do "&nbsp;Section&nbsp;" end
-                                    select.att_section!.att_input :name => "att_section" do
-                                             option :value => "general", :default => "true" do "General" end
-                                             option :value => "teleop" do "TeleOp" end
-                                             option :value => "autonomous" do "Autonomous" end
-                                           end
-                                    label :for => "att_type" do "&nbsp;Type&nbsp;" end
-                                    select.att_type!.att_input :name => "att_type" do
-                                             option :value => "string", :default => "true" do "String" end
-                                             option :value => "picklist" do "Picklist" end
-                                             option :value => "radio" do "Radio button" end
-                                           end
-                                    br
-                                    label :for => "att_options" do "&nbsp;Options&nbsp;" end
-                                    br
-                                    textarea.att_options!.att_input :name => "att_options"
-                                    input :type => "submit", :value => "Add"
-                                  end
+      if (@attributes.autonomous.exists?)
+        h2 "Autonomous"
+        @attributes.autonomous.each do |at|
+                                p do
+                                  text "#{at.name} (#{at._id}) / #{at.type}"
                                 end
+                              end
+      end
+      if (@attributes.general.exists?)
+        h2 "General"
+        @attributes.general.each do |at|
+                             p do
+                               text "#{at.name} (#{at._id}) / #{at.type}"
+                             end
+                           end
+      end
+
+      form :action => R(Attributes), :method => "post" do
+                                       p do
+                                         label :for => "att_name" do "Name&nbsp;" end
+                                         input.att_name!.att_input :name => "att_name", :type => "text"
+                                         label :for => "att_group" do "&nbsp;Group&nbsp;" end
+                                         select.att_group!.att_input :name => "att_group" do
+                                                  option :value => "general", :default => "true" do "General" end
+                                                  option :value => "teleop" do "TeleOp" end
+                                                  option :value => "autonomous" do "Autonomous" end
+                                                end
+                                         label :for => "att_type" do "&nbsp;Type&nbsp;" end
+                                         select.att_type!.att_input :name => "att_type" do
+                                                  option :value => "string", :default => "true" do "String" end
+                                                  option :value => "picklist" do "Picklist" end
+                                                  option :value => "radio" do "Radio button" end
+                                                  option :value => "checkbox" do "Checkbox" end
+                                                end
+                                         br
+                                         label :for => "att_options" do "&nbsp;Options&nbsp;" end
+                                         br
+                                         textarea.att_options!.att_input :name => "att_options"
+                                         input :type => "submit", :value => "Add"
+                                       end
+                                     end
     end
-    
-    def quick_take_options
-      option :value => "unknown", :default => "true" do "Not scouted" end
-      option :value => "must" do "MUST pick" end
-      option :value => "fair" do "Fair pick" end
-      option :value => "poor" do "Poor pick" end
-      option :value => "nooo" do "DO NOT pick" end
+  end
+
+  module Helpers
+    def attribute_input(at, team, edit)
+      if edit
+        case at.type
+        when 'picklist'
+          label :for => at._id do "#{at.name}&nbsp;" end
+          select :id => at._id, :name => at._id do
+                            option :value => "unknown", :default => "true"
+                            at.options.each do |opt|
+                                        if (team.attrs[at._id] == opt['value'])
+                                          option :value => opt['value'], :selected => 'true' do opt['text'] end
+                                        else
+                                          option :value => opt['value'] do opt['text'] end
+                                        end
+                                      end
+                          end
+        when 'radio'
+          label at.name
+          at.options.each do |opt|
+                      if (team.attrs[at._id] == opt['value'])
+                        input :type => "radio", 
+                        :id => "#{at._id}_#{opt['value']}", 
+                        :name => at._id,
+                        :value => opt['value'],
+                        :checked => 'true'
+                        label :for => "#{at._id}_#{opt['value']}" do opt['text'] end
+                      else
+                        input :type => "radio", 
+                        :id => "#{at._id}_#{opt['value']}", 
+                        :name => at._id,
+                        :value => opt['value']
+                        label :for => "#{at._id}_#{opt['value']}" do opt['text'] end
+                      end
+                    end
+        when 'checkbox'
+          if (team.attrs[at._id] == 'yes')
+            input :type => 'checkbox', :value => 'yes', :id => at._id, :name => at._id, :checked => 'true'
+          else
+            input :type => 'checkbox', :value => 'yes', :id => at._id, :name => at._id
+          end
+          label :for => at._id do at.name end
+        else
+          label :for => at._id do "#{at.name}&nbsp;" end
+          input :type => 'text', :id => at._id, :name => at._id, :value => team.attrs[at._id]
+        end
+      else
+        value = ''
+        if (at.type == 'string')
+          value = team.attrs[at._id]
+        elsif (at.type == 'checkbox')
+          value = team.attrs[at._id] == 'yes' ? 'yes' : 'no'
+        else
+          at.options.each do |opt|
+                      if opt['value'] == team.attrs[at._id]
+                        value = opt['text']
+                        break
+                      end
+                    end
+        end
+        text "#{at.name}: #{value}"
+      end
     end
   end
 end
